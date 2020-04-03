7516c836b81771713a2a95d11a0692e8573c3929
yannick <yw214960@fh-muenster.de>
2020-04-03 19:23:56 +0200

    Models geändert

--- MV.Web/Models/Membership.cs
+++ MV.Web/Models/Membership.cs
@@ -1,10 +1,19 @@
 ﻿
 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MV.Web.Models
 {
+  public enum MembershipState
+  {
+    Active,
+    Closed
+  }
+
+
   public class Membership
   {
     public Membership()
@@ -12,6 +21,12 @@ namespace MV.Web.Models
     }
 
     public int ID { get; set; }
+
+    [DisplayName("Eintritsdatum"), DataType(DataType.Date)]
+    public DateTime CreationDate { get; set; }
+
+    [DisplayName("Status")]
+    public MembershipState State { get; set; }
 
     [DisplayName("Mitglieds Nr")]
     public int MembershipNo { get; set; }
--- MV.Web/Models/Person.cs
+++ MV.Web/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MV.Web.Models
 {
@@ -9,20 +10,38 @@ namespace MV.Web.Models
     {
     }
     public int ID { get; set; }
+
+    [StringLength(25)]
     [DisplayName("Vorname")]
     public string Firstname { get; set; }
+
+    [Required, StringLength(25)]
     public string Surname { get; set; }
+
+    [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd.MM.yy}")]
     public DateTime DateOfBirth { get; set; }
 
     // ---- contact ------
+    [EmailAddress]
     public string Email { get; set; }
+    [Phone]
     public string Mobilephone { get; set; }
+    [Phone(ErrorMessage = "The fixed line phone number is not well formatted")]
     public string Hometelephone { get; set; }
 
     // ---- address ------
     public string Street { get; set; }
+
+    [StringLength(5)]
     public string Housenumber { get; set; }
-    public int Zipcode { get; set; }
+
+    [StringLength(3)]
+    public string IsoCountryCode { get; set; }
+
+    [StringLength(10)]
+    public string Zipcode { get; set; }
+
+    [StringLength(35)]
     public string City { get; set; }
 
     public int MembershipID { get; set; }
